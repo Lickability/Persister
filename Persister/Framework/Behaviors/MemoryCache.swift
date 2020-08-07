c09621f6445c601ae025397883ee6eb6a014abcc
Michael Liberatore <mi@chael.co>
2020-08-07 14:09:36 -0400

    Memory Cache docs + updates

--- Persister/Framework/Behaviors/MemoryCache.swift
+++ Persister/Framework/Behaviors/MemoryCache.swift
@@ -8,19 +8,24 @@
 
 import Foundation
 
+/// Describes a type capable of caching items in memory.
 public struct MemoryCache {
     
     // MARK: - Cache
     
-    public let expirationInterval: TimeInterval
+    public let expiration: CacheExpiration
     
     // MARK: - MemoryCache
     
     private let cache: LRUCache<String, Any>
     
-    public init(cache: LRUCache<String, Any>, expirationInterval: TimeInterval = 600) {
+    /// Creates a new `MemoryCache`.
+    /// - Parameters:
+    ///   - cache: The underlying cache used to store / recall items.
+    ///   - expiration: Determines when newly written items are considered expired. Defaults to expire items in 10 minutes (600 seconds).
+    public init(cache: LRUCache<String, Any>, expiration: CacheExpiration = .afterInterval(600)) {
         self.cache = cache
-        self.expirationInterval = expirationInterval
+        self.expiration = expiration
     }
 }
 
@@ -33,6 +38,18 @@ extension MemoryCache: Cache {
     }
     
     public func write<T: Codable>(item: T, forKey key: String) throws {
-        cache.set(item, for: key, expiresOn: Date().addingTimeInterval(expirationInterval))
+        cache.set(item, for: key, expiresOn: expiration.expirationDate(from: Date()))
+    }
+}
+
+private extension CacheExpiration {
+    
+    func expirationDate(from date: Date) -> Date? {
+        switch self {
+        case .never:
+            return nil
+        case .afterInterval(let interval):
+            return date.addingTimeInterval(interval)
+        }
     }
 }
